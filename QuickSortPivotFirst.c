0ef7796f9a1e0bf731d15e64470bb71cc457ad43
Shahriar Jubair <shahriarjubair27@gmail.com>
2024-05-16 22:12:26 +0600

    dddd

---
+++ QuickSortPivotFirst.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i >= j) {
+        while (i <= high && arr[i] <= pivot) {
+            i++;
+        }
+        while (j >= low && arr[j] > pivot) {
+            j--;
+        }
+        swap(&arr[i], &arr[j]);
+    }
+    swap(&pivot, &arr[j]);
+
+    return j;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: \n");
+    printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
